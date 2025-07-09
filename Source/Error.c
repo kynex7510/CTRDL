cf26a7d3bb7919ffcc5ad824ea716d4316251b65
kynex7510 <nikkitten3@gmail.com>
2025-07-09 10:49:21 +0200

    Update license

--- Source/Error.c
+++ Source/Error.c
@@ -1,3 +1,9 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include "Error.h"
 
 static __thread size_t g_LastError = (size_t)Err_OK;
